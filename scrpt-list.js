225ea849dbe6a21e82466abfc43260e674a6266c
MoldyCabbage <vanessa.retz1331@gmail.com>
2023-04-04 22:28:55 +0200

    Update scrpt-list.js

--- scrpt-list.js
+++ scrpt-list.js
@@ -3,8 +3,9 @@ var input = document.getElementById('userInput');
 var ul = document.querySelector('ul');
 var item = document.getElementsByTagName('li');
 
+// Get length of input
 let inputLength = () => input.value.length;
-
+// Get current length of list 
 let listLength = () => item.length;
 
 // Create a new list element 
@@ -19,14 +20,13 @@ const createListElement = () => {
     // Reset text input field
     input.value= '';
 
-
-
     // -- Toggle if done -- 
     const crossOut = e => {
         li.classList.toggle('done');
     }
     li.addEventListener('click', crossOut, false);
     // -- Toggle if done -- 
+
 
     // -- Adding Delete button --
     const deleteListItem = e => {
@@ -38,7 +38,6 @@ const createListElement = () => {
     li.appendChild(btn);
     btn.addEventListener('click', deleteListItem, false);
     // -- Adding Delete button --
-
 }
 
 // Event handler if clicked or 'enter' is pressed
